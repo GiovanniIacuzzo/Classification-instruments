91ba35b13e90d2d20dce548413bdc1cccdc56100
GiovanniIacuzzo <iacuzzogiovanni02@gmail.com>
2025-04-12 12:28:06 +0200

    rimozione

--- main.py
+++
@@ -1,6 +0,0 @@
-from utils.train_audio import train_model
-from utils.evaluate_audio import evaluate_model
-
-if __name__ == "__main__":
-    train_model("./data", epochs=10, batch_size=16, lr=0.001)
-    # evaluate_model("dataset_split/test")
